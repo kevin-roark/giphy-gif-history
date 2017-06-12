f334db768ebc01a79476915386f4cea7559535a5
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-06-12 16:01:07 -0400

    nicer splash

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -2,17 +2,27 @@
   <div class="splash" :style="splashStyle">
     <h1 class="title">History of the GIF</h1>
 
-    <h2 class="presented-by">Presented By</h2>
-    <img class="logo giphy-arts" src="static/img/giphy-arts.png" alt="Giphy Arts" />
+    <div class="enter-container">
+      <div>
+        <h2 class="presented-by">Presented By</h2>
+        <div class="logos">
+          <a class="logo" href="https://giphy.com/giphyarts" target="_blank">
+            <img class="giphy-arts" src="static/img/giphy-arts-logo.png" alt="Giphy Arts" />
+          </a>
+          <a class="logo" href="https://computerlab.io" target="_blank">
+            <img class="computer-lab" src="static/img/computer-lab-logo.png" alt="Computer Lab" />
+          </a>
+        </div>
+      </div>
 
-    <div class="timeline-link" @click="onEnterClick">ENTER</div>
+      <div class="timeline-link" @click="onEnterClick">ENTER</div>
+    </div>
 
     <div class="credits">
       <ul class="credits-list">
         <li>Text by: Celine</li>
         <li>
           Website by:
-          <a href="https://computerlab.io/" target="_blank">Computer Lab</a> &amp;
           <a href="http://www.kevinroark.com" target="_blank">Kevin Roark</a>
         </li>
       </ul>
@@ -33,7 +43,7 @@ export default {
     let hue = 0
     this.rainbowInterval = setInterval(() => {
       hue += 0.002
-      color.setHSL(hue, 1, 0.75)
+      color.setHSL(hue, 1, 0.6)
       this.rainbowColor = { r: Math.round(color.r * 255), g: Math.round(color.g * 255), b: Math.round(color.b * 255) }
 
       if (hue >= 1) {
@@ -51,6 +61,11 @@ export default {
       let bgColor = `rgba(${c.r}, ${c.g}, ${c.b}, 0.25)`
       return { background: bgColor, border: `2px solid ${color}` }
     }
+    // enterStyle () {
+    //   let c = this.rainbowColor
+    //   let color = `rgb(${c.r}, ${c.g}, ${c.b})`
+    //   return { background: color }
+    // }
   },
   methods: {
     onEnterClick () {
@@ -62,38 +77,61 @@ export default {
 
 <style scoped>
 .splash {
-  box-sizing: border-box;
   position: fixed;
-  top: 0; left: 0;
-  width: calc(100% - 100px);
-  margin: 50px;
-  padding: 20px;
+  top: 50px; left: 50%;
+  transform: translateX(-50%);
+  padding: 20px 20px 20px 40px;
   border-radius: 4px;
   box-shadow: 0 2px 6px 0 rgba(0,0,0,0.80);
 }
 
 .title {
-  margin: 0 0 40px 0;
+  margin: 0 40px 0 40px;
   padding: 0;
   font-family: 'FuturaBT-Bold', Menlo, WorkSans-Regular, Helvetica, Arial, sans-serif;
-  font-size: 72px;
+  font-size: 84px;
   font-weight: normal;
+  white-space: pre;
+}
+
+.enter-container {
+  margin-top: 40px;
+  text-align: center;
 }
 
 .presented-by {
-  margin: 0 0 20px 0;
+  margin: 0;
   padding: 0;
   font-family: 'FuturaBT-Bold', Menlo, WorkSans-Regular, Helvetica, Arial, sans-serif;
-  font-size: 28px;
+  font-size: 32px;
   font-weight: normal;
-  color: #111;
+  color: #eee;
+  text-decoration: underline;
+}
+
+.logos {
+  margin: 40px 0 20px 0;
+  vertical-align: top;
+}
+
+.logo {
+  display: inline-block;
+  margin: 0 0 0 0;
+  padding: 0;
+  position: relative;
+  vertical-align: top;
+}
+
+.logo img {
+  height: 244px;
 }
 
 .giphy-arts {
-  display: block;
-  width: 300px;
-  margin: 0 0 40px 0;
-  padding: 0;
+  transform: translate(-15px, -16px);
+}
+
+img.computer-lab {
+  height: 220px;
 }
 
 .credits {
@@ -114,21 +152,20 @@ export default {
 
 .timeline-link {
   display: inline-block;
-  margin-left: 50%;
-  transform: translateX(-50%);
-  padding: 20px;
+  padding: 20px 80px;
   border-radius: 4px;
   text-align: center;
-  background: #FFF35C;
-  color: #222;
+  background: #000;
+  color: #fff;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  border: 2px solid #000;
+  border: 2px solid transparent;
   font-family: 'FuturaBT-Bold', Menlo, WorkSans-Regular, Helvetica, Arial, sans-serif;
   font-size: 36px;
+  letter-spacing: 1 px;
   cursor: pointer;
-  transition: box-shadow 0.2s;
 }
   .timeline-link:hover {
+    border: 2px solid #fff;
     box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   }
 </style>
